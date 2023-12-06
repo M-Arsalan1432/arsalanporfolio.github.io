9c499e1a704fe48d80c710623c4dec4cb92358d0
M-Arsalan1432 <109943967+M-Arsalan1432@users.noreply.github.com>
2023-12-06 12:46:18 +0500

    arsalan portfolio

---
+++ assets/vendor/php-email-form/validate.js
@@ -0,0 +1,149 @@
+
+// (function () {
+//   "use strict";
+
+//   let forms = document.querySelectorAll('.php-email-form');
+
+//   forms.forEach( function(e) {
+//     e.addEventListener('submit', function(event) {
+//       event.preventDefault();
+
+//       let thisForm = this;
+
+//       let action = thisForm.getAttribute('action');
+//       let recaptcha = thisForm.getAttribute('data-recaptcha-site-key');
+      
+//       if( ! action ) {
+//         displayError(thisForm, 'The form action property is not set!');
+//         return;
+//       }
+//       thisForm.querySelector('.loading').classList.add('d-block');
+//       thisForm.querySelector('.error-message').classList.remove('d-block');
+//       thisForm.querySelector('.sent-message').classList.remove('d-block');
+
+//       let formData = new FormData( thisForm );
+
+//       if ( recaptcha ) {
+//         if(typeof grecaptcha !== "undefined" ) {
+//           grecaptcha.ready(function() {
+//             try {
+//               grecaptcha.execute(recaptcha, {action: 'php_email_form_submit'})
+//               .then(token => {
+//                 formData.set('recaptcha-response', token);
+//                 php_email_form_submit(thisForm, action, formData);
+//               })
+//             } catch(error) {
+//               displayError(thisForm, error);
+//             }
+//           });
+//         } else {
+//           displayError(thisForm, 'The reCaptcha javascript API url is not loaded!')
+//         }
+//       } else {
+//         php_email_form_submit(thisForm, action, formData);
+//       }
+//     });
+//   });
+
+//   function php_email_form_submit(thisForm, action, formData) {
+//     fetch(action, {
+//       method: 'POST',
+//       body: formData,
+//       headers: {'X-Requested-With': 'XMLHttpRequest'}
+//     })
+//     .then(response => {
+//       if( response.ok ) {
+//         return response.text();
+//       } else {
+//         throw new Error(`${response.status} ${response.statusText} ${response.url}`); 
+//       }
+//     })
+//     .then(data => {
+//       thisForm.querySelector('.loading').classList.remove('d-block');
+//       if (data.trim() == 'OK') {
+//         thisForm.querySelector('.sent-message').classList.add('d-block');
+//         thisForm.reset(); 
+//       } else {
+//         throw new Error(data ? data : 'Form submission failed and no error message returned from: ' + action); 
+//       }
+//     })
+//     .catch((error) => {
+//       displayError(thisForm, error);
+//     });
+//   }
+
+//   function displayError(thisForm, error) {
+//     thisForm.querySelector('.loading').classList.remove('d-block');
+//     thisForm.querySelector('.error-message').innerHTML = error;
+//     thisForm.querySelector('.error-message').classList.add('d-block');
+//   }
+
+// })();
+
+
+(function () {
+  "use strict";
+
+  let forms = document.querySelectorAll('.php-email-form');
+
+  forms.forEach(function (form) {
+    form.addEventListener('submit', function (event) {
+      event.preventDefault();
+
+      let action = form.getAttribute('action');
+      let recaptcha = form.getAttribute('data-recaptcha-site-key');
+
+      if (!action) {
+        displayError(form, 'The form action property is not set!');
+        return;
+      }
+
+      form.querySelector('.loading').classList.add('d-block');
+      form.querySelector('.error-message').classList.remove('d-block');
+      form.querySelector('.sent-message').classList.remove('d-block');
+
+      let formData = new FormData(form);
+
+      if (recaptcha) {
+        // Handling for reCaptcha, if needed
+        // ...
+      } else {
+        phpEmailFormSubmit(form, action, formData);
+      }
+    });
+  });
+
+  function phpEmailFormSubmit(form, action, formData) {
+    fetch(action, {
+      method: 'POST',
+      body: formData,
+      headers: { 'X-Requested-With': 'XMLHttpRequest' }
+    })
+      .then(response => {
+        if (response.ok) {
+          return response.text();
+        } else {
+          throw new Error(`${response.status} ${response.statusText} ${response.url}`);
+        }
+      })
+      .then(data => {
+        form.querySelector('.loading').classList.remove('d-block');
+        if (data.trim() === 'OK') {
+          form.querySelector('.sent-message').classList.add('d-block');
+          form.reset();
+        } else {
+          throw new Error(data ? data : 'Form submission failed and no error message returned from: ' + action);
+        }
+      })
+      .catch((error) => {
+        displayError(form, error);
+      });
+  }
+
+  function displayError(form, error) {
+    form.querySelector('.loading').classList.remove('d-block');
+    form.querySelector('.error-message').innerHTML = error;
+    form.querySelector('.error-message').classList.add('d-block');
+  }
+
+})();
